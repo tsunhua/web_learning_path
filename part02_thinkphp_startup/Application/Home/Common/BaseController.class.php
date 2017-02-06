ea105b69b0abc386680adbd68d03d47ec06fd85e
Lshare <linlshare@gmail.com>
2017-02-06 19:57:16 +0800

    tp startup

---
+++ part02_thinkphp_startup/Application/Home/Common/BaseController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Lshare
+ * Date: 2017/2/6
+ * Time: 17:02
+ */
+//注意命名空间要跟
+namespace Home\Common;
+use \Think\Controller;
+
+class BaseController extends Controller
+{
+    private function getUrlVar()
+    {
+        return I('param.');
+    }
+
+}
